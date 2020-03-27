c5f4bba027c5755b9c39a466ed29759ceb23cf84
Ankitgif <ankitsinha2303@gmail.com>
2020-03-27 16:30:53 +0530

    Added optional part validating special character proceeding to xyz

--- email.sh
+++ email.sh
@@ -2,8 +2,8 @@
 shopt -s extglob
 echo "Enter email id : "
 read mailid
-pat="^[a-z]{3,}[@]{1}[a-z]{4,}[.]{1}[a-z]{2,4}"
-if [[ $mailid =~  $pat ]]
+pat="^[a-z]{3,}([._+\-][a-z0-9]*)?[@]{1}[a-z]{3,}[.]{1}[a-z]{2,4}$"
+if [[ $mailid =~ $pat ]]
 then
 	echo valid;
 else
