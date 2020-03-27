75886351361da1c52e31e90d96059e5ba2013960
Ankitgif <ankitsinha2303@gmail.com>
2020-03-27 13:11:40 +0530

    Added '.' after bridgelabz and validated the mandotory 3ed part i.e co

--- email.sh
+++ email.sh
@@ -2,7 +2,7 @@
 shopt -s extglob
 echo "Enter email id : "
 read mailid
-pat="^[a-z]{3,}[@]{1}[a-z]{4,}"
+pat="^[a-z]{3,}[@]{1}[a-z]{4,}[.]{1}[a-z]{2,4}"
 if [[ $mailid =~  $pat ]]
 then
 	echo valid;
