a14cb0f3a15661302f96782d731e5ae66e548489
Ankitgif <ankitsinha2303@gmail.com>
2020-03-27 16:44:12 +0530

    Added last part which is the optional country code and its 2 characters only

--- email.sh
+++ email.sh
@@ -2,7 +2,7 @@
 shopt -s extglob
 echo "Enter email id : "
 read mailid
-pat="^[a-z]{3,}([._+\-][a-z0-9]*)?[@]{1}[a-z]{3,}[.]{1}[a-z]{2,4}$"
+pat="^[a-z]{3,}([._+\-][a-z0-9]*)?[@]{1}[a-z]{3,}[.]{1}[a-z]{2,4}(\.[a-z]{2})?$"
 if [[ $mailid =~ $pat ]]
 then
 	echo valid;
